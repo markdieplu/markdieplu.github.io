b865266a769bfcd31d2382307e0358d3c6a56e00
markdieplu <67131004+markdieplu@users.noreply.github.com>
2021-04-21 23:42:04 -0400

    Timer Test v.03

--- timer.js
+++ timer.js
@@ -1,22 +1,32 @@
-function toTimeString(seconds) {
-  return (new Date(seconds * 1000)).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0];
-}
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
 
-function startTimer() {
-  var nextElem = $(this).parents('td').next();
-  var duration = nextElem.text();
-  var a = duration.split(':');
-  var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
-  setInterval(function() {
-    seconds--;
-    if (seconds >= 0) {
-      nextElem.html(toTimeString(seconds));
-    }
-    if (seconds === 0) {
-    	document.getElementById("myBtn").disabled = true;
-      alert('You made it!');
-      clearInterval(seconds);
-    }
-  }, 1000);
-}
-$('.btn').on('click', startTimer);
+  let birthday = "Sep 30, 2021 00:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
+
+          headline.innerText = "whew~ o-o";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
